6d455cac6431659913312101977e19b126fd5227
Kevin Geary <kgeary@gmail.com>
2020-02-26 19:53:18 -0600

    Updates

---
+++ precache-manifest.c4fa5538d4d8742588a2d95dd09007a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c12dadbc7023ec5833d563996ebfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "674a5ef8a12d7d963282",
+    "url": "/static/css/2.d0a7bb6d.chunk.css"
+  },
+  {
+    "revision": "2d5ffd08f9d508995cf4",
+    "url": "/static/css/main.08f59e7b.chunk.css"
+  },
+  {
+    "revision": "674a5ef8a12d7d963282",
+    "url": "/static/js/2.e907ce7d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e907ce7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5ffd08f9d508995cf4",
+    "url": "/static/js/main.ce401cec.chunk.js"
+  },
+  {
+    "revision": "e3801edb42b2e1b76aaf",
+    "url": "/static/js/runtime-main.54bdaecd.js"
+  }
+]);
